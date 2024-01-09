e2b0bb23d4a7b610e6f349ceecbb33c02ed8a61d
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-01-09 09:16:20 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #python scripts/0_precalculate_proteins.py
 #python scripts/0_precalculate_proteins_only_slc_aucs.py
+<<<<<<< Updated upstream
 python scripts/0_precalculate_proteins_baseline.py
+=======
+>>>>>>> Stashed changes
 #python scripts/1_experimental_validation_predictions.py
-#python scripts/1_gsf_predictions.py
+python scripts/1_gsf_predictions.py
 #python scripts/1_chembl_predictions.py
